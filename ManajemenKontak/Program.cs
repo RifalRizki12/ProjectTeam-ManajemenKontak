e943b5a4bed972f32ce909cf5f6a95f85a9c50c1
Lailatul Qomariah <55173357+Lailatul-qomariah@users.noreply.github.com>
2023-09-15 11:40:19 +0700

    MainMenu
    Main menu update

--- ManajemenKontak/Program.cs
+++ ManajemenKontak/Program.cs
@@ -6,37 +6,98 @@ class Program
 {
     public static void Main(string[] args)
     {
-                
+        MainMenu();         
     }
 
-    public void MainMenu()
-    {
+    public static void MainMenu()
+    {   
+        
         while (true)
         {
             Console.Clear();
 
-            Console.WriteLine("==========================");
-            Console.WriteLine("\t Menu Managemen Kontak \t");
-            Console.WriteLine("==========================");
-            Console.WriteLine("1. Manajemen Kontak");
+            Console.WriteLine("==========================================");
+            Console.WriteLine("\t Menu Management Contact \t");
+            Console.WriteLine("==========================================");
+            Console.WriteLine("1. Management Contact");
             Console.WriteLine("2. Recycle bin kontak");
             Console.WriteLine("3. Exit");
             Console.Write("\nMasukkan Pilihan : ");
-            string choice = Console.ReadLine();
+            string choiceMain = Console.ReadLine();
 
-            switch (choice)
+            switch (choiceMain)
             {
                 case "1":
+                    Console.Clear();
                     Console.WriteLine("==========================");
-                    Console.WriteLine("\t Managemen Kontak \t");
+                    Console.WriteLine("\t MANAGEMENT CONTACT \t");
                     Console.WriteLine("==========================");
                     Console.WriteLine("1. View Kontak");
                     Console.WriteLine("2. Create kontak");
-                    Console.WriteLine("3. Update Kontak");
-                    Console.WriteLine("4. Delete Kontak");
-                    Console.WriteLine("5. Search Kontak");
+                    Console.WriteLine("3. Search Kontak");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("Input : ");
+                    string choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                            Console.Clear();
+                            Console.WriteLine("----- View Kontak -----");
+
+                            Console.WriteLine("1. Edit Kontak");
+                            Console.WriteLine("2. Delete Kontak");
+                            Console.WriteLine("3. Back Kontak");
+                            string choiceCrud = Console.ReadLine();
+                            
+
+
+                            Console.ReadLine();
+                            switch (choiceCrud)
+                            {
+                                case "1":
+                                    Console.WriteLine("------- Edit Kontak -------");
+                                    Console.Write("Masukkan Nama : ");
+                                    Console.ReadLine();
+
+
+                                    break;
+                                case "2":
+                                    Console.WriteLine("------- Delete Kontak -------");
+                                    break;
+                                case "3":
+                                    break;
+
+                            }
+                            break;
+                        case "2":
+                            Console.Clear();
+                            Console.WriteLine("----- Create Kontak -----");
+                            Console.ReadLine();
+                            break;
+                        case "3":
+                            Console.Clear();
+                            Console.WriteLine("----- Search Kontak -----");
+                            Console.ReadLine();
+                            break;
+                        case "4":
+                            break;
+                    }
 
                     break;
+
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("RECYCLE BIN CONTACT");
+                    Console.ReadLine();
+                    break;
+                case "3":
+                Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Enter options correctly");
+                    Console.ReadLine();
+                    break;
+
             }
         }
     }
